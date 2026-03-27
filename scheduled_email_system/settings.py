030ce4e04ec5870f923d47267902db97e45dda53
Yogesh B <ybodke123@gmail.com>
2026-03-27 15:03:45 +0530

    pw

--- scheduled_email_system/settings.py
+++ scheduled_email_system/settings.py
@@ -147,4 +147,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'ybodke123@gmail.com'
-EMAIL_HOST_PASSWORD = 'jnds xhms wtfz zifx'
+# EMAIL_HOST_PASSWORD = 'jnds xhms wtfz zifx'
+EMAIL_HOST_PASSWORD = 'password'
